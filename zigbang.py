f91f66a2101a2a986abfe7a52860fbb7de300b75
JunKiBeom <dona0408@gmail.com>
2023-04-29 03:43:42 +0900

    Zigbang v1

--- zigbang.py
+++ zigbang.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 import time, csv
@@ -7,14 +8,20 @@ import time, csv
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
-url = "https://www.zigbang.com/home/oneroom/items/36431738"
+url = "https://www.zigbang.com/home/oneroom/items/"
 
-driver.get(url)
-time.sleep(1)
+for i in range(36431730, 36431740):
+    driver.get(url+str(i))
+    # time.sleep(1)
 
-html = driver.page_source
-parse = BeautifulSoup(html, 'html.parser')
-print(parse)
+    try:
+        err = driver.find_element(By.XPATH, '/html/body/div[7]/div/div[2]/div/div/div/div[2]')
+    except:
+        tmp = driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/div/div[2]/div/div/div[2]')
+        html = driver.page_source
+        parse = BeautifulSoup(html, 'html.parser')
+        print(tmp.text)
+        print('-'*30)
 
 driver.close()
 
