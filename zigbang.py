5f6b15f35c5ec21b6ab91b6eeca48db68f9eb9a1
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2023-04-28 11:26:48 +0900

    parse test

--- zigbang.py
+++ zigbang.py
@@ -1,14 +1,21 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from webdriver_manager import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 
 import time, csv
 
 options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(ChromeDriverManager.install(), options)
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 url = "https://www.zigbang.com/home/oneroom/items/36431738"
 
 driver.get(url)
 time.sleep(1)
+
+html = driver.page_source
+parse = BeautifulSoup(html, 'html.parser')
+print(parse)
+
 driver.close()
+
+# //*[@id="__next"]/div[2]/div/div[2]/div/div/div[2]
