b660d2e09af0a464d795555417f4c7a530b1f8fd
JunKiBeom <dona0408@gmail.com>
2023-05-14 18:56:29 +0900

    네이버 크롤링 통합 함수 생성

--- capstone_webproject/rpa/naver2.py
+++ capstone_webproject/rpa/naver2.py
@@ -15,10 +15,12 @@ cortarNo = 0
 
 addr = ""
 
+
 def mkurl():
     url2 = f'https://new.land.naver.com/api/articles?cortarNo={cortarNo}&order=rank&realEstateType=VL:DDDGG:JWJT:SGJT:HOJT&tradeType=B1:B2&tag=::::::::&rentPriceMin=0&rentPriceMax=900000000&priceMin=0&priceMax=900000000&areaMin=0&areaMax=900000000&oldBuildYears&recentlyBuildYears&minHouseHoldCount&maxHouseHoldCount&showArticle=false&sameAddressGroup=false&minMaintenanceCost&maxMaintenanceCost&priceType=RETAIL&directions=&page={page}&articleState'
     url3 = f'https://new.land.naver.com/api/articles?cortarNo={cortarNo}&order=rank&realEstateType=APT:OPST:ABYG:OBYG:GM:OR:VL:DDDGG:JWJT:SGJT:HOJT&tradeType=B1:B2&tag=:::::::SMALLSPCRENT:&rentPriceMin=0&rentPriceMax=900000000&priceMin=0&priceMax=900000000&areaMin=0&areaMax=900000000&oldBuildYears&recentlyBuildYears&minHouseHoldCount&maxHouseHoldCount&showArticle=false&sameAddressGroup=false&minMaintenanceCost&maxMaintenanceCost&priceType=RETAIL&directions=&page={page}&articleState'
     return url2, url3
+
 
 def mkdf(url, flag):
     response2 = requests.get(url, headers=params)  # 매물정보를 가져오기. 진짜 크롤링을 하는 부분
@@ -67,6 +69,7 @@ params = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
 }  # 필수 Request header. 없으면 캡챠 에러 발생, authorization은 주기적으로 바뀐다고 함
 
+
 def get_addr(addrs):
     try:
         global addr
@@ -85,9 +88,14 @@ def get_addr(addrs):
     global cortarNo
     cortarNo = response.json()["cortarNo"]
 
-'''
-for loop으로 돌려도 되지만 2개 뿐이라 시간복잡도 이득을 위해 그냥 2번 호출
-'''
-# urls = mkurl()
-# mkdf(urls[0], 0)
-# mkdf(urls[1], 1)
+
+def naver(addr):
+    get_addr(addr)
+    urls = mkurl()
+    '''
+    for loop으로 돌려도 되지만 2개 뿐이라 시간복잡도 이득을 위해 그냥 2번 호출
+    '''
+    mkdf(urls[0], 0)
+    mkdf(urls[1], 1)
+
+# naver("서울시 마포구 연남동")
