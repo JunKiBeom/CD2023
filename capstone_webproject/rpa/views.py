34b97bc841915481f25ab0a4fd819adcb0031a55
JunKiBeom <dona0408@gmail.com>
2023-05-14 20:30:21 +0900

    크롤링 함수 연결

--- capstone_webproject/rpa/views.py
+++ capstone_webproject/rpa/views.py
@@ -1,20 +1,29 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from . import naver2, zigbang
 
+
 def index(request):
     return render(request, 'index.html')
+
+
 def home(request):
     return render(request, 'homealldata.html')
 
+
 def test_home(request):
     return render(request, 'test_home.html')
+
+
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'product_list.html', {'products': products})
 
+
 def product_detail(request, pk):
     product = Product.objects.get(pk=pk)
     return render(request, 'product_detail.html', {'product': product})
+
 
 def chart(request):
     monthly_rental_data = Property.objects.filter(rent_type='월세')
@@ -25,14 +34,18 @@ def chart(request):
     # jeonse_data에 대한 전세 차트 표시
     # monthly_jeonse_data에 대한 월세+전세 차트 표시
 
+
 def addr_get(request):
     city = request.POST.get('city')
     district = request.POST.get('district')
     address = request.POST.get('address')
-    # print(city, district, address)
+    # print(f'"{city}", "{district}", "{address}"')
     result = city + " " + district + " " + address
 
-    # naver2(result)
-    # zigbang.zigbang(result)
-
+    if city == "" or address == "":
+        pass
+    else:
+        zigbang.zigbang(result)
+        naver2.naver(result)
     return redirect('/')
+    # return render(request, 'homealldata.html')
