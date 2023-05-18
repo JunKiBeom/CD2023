4a158ac9630d324840e08655d6d2b4c5dbb22dbd
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2023-05-18 12:49:40 +0900

    admin 페이지에서 db관리 추가

--- capstone_webproject/rpa/admin.py
+++ capstone_webproject/rpa/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from .models import Product
 
 # Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['address', 'flag', 'item_id', 'service_type', 'sales_type']
+    list_filter = ['address', 'flag', 'service_type', 'sales_type']
+
+admin.site.register(Product, ProductAdmin)
