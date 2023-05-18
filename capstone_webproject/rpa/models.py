73c207f489fb117a549e3a4e95a30fcbe2cc687d
전기범 <37178265+JunKiBeom@users.noreply.github.com>
2023-05-18 12:52:43 +0900

    audo input generate date in data

--- capstone_webproject/rpa/models.py
+++ capstone_webproject/rpa/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+
 
 class Product(models.Model):
     # name = models.CharField(max_length=100)
@@ -20,6 +22,10 @@ class Product(models.Model):
     date = models.CharField(max_length=8)
     # latitude = models.
     # longitude = models.
+    gen_date = models.DateField(default = timezone.now)
 
     class Meta:
-        unique_together = (("flag", "item_id"),)
+        unique_together = ("flag", "item_id")
+
+    def __str__(self):
+        return str(self.flag) + str(self.item_id) + str(self.address)
