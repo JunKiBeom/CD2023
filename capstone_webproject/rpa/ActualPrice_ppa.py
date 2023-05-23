29eb21b1f808056203434f043873ef390402ddf5
jude7794 <jude7794@naver.com>
2023-05-24 03:23:49 +0900

    실거래가 도 별로 나눈 파일 가격점수, ppa 처리

---
+++ capstone_webproject/rpa/ActualPrice_ppa.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import os
+import re
+
+df = pd.read_csv('/content/경기도_실거래가.csv', encoding='cp949')
+print(df)
+
+# ppa, 가격점수
+## 전세, 월세 가격 통일(가격 점수)
+df["price_score"] = 0
+df["보증금(만원)"] = df["보증금(만원)"].str.replace(',','')
+df["월세(만원)"] = df["월세(만원)"].astype(str).str.replace(',','')
+deposit = df["보증금(만원)"].apply(float)
+month = df["월세(만원)"].apply(float)
+df.loc[df["전월세구분"] == "월세", "price_score"] = deposit + month*100
+df.loc[df["전월세구분"] == "전세", "price_score"] = deposit
+
+## 면적 당 가격(price per area)
+df["ppa"] = 0
+df["전용면적(㎡)"] = df["전용면적(㎡)"].apply(float)
+df["ppa"] = round(df["price_score"] / df["전용면적(㎡)"], 2)
+
+print(df)
+
+df.to_csv('경기도_실거래가(ppa).csv', encoding='euc-kr', index = None)
+
+
+
+
+
